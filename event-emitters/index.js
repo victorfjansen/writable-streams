131ac1454272c31fbac4cd0afacea8b12a238707
= <victorfjansen@gmail.com>
2025-02-07 07:25:46 -0300

    feat: addd EventEmitters example

---
+++ event-emitters/index.js
@@ -0,0 +1,12 @@
+const EventEmitter = require("events");
+
+const commandHandler = new EventEmitter();
+
+// observer
+commandHandler.on("myEvent", (myCallbackValue) => {
+    console.log("Log Here: ", myCallbackValue);
+})
+
+// producer
+setTimeout(() =>
+    commandHandler.emit("myEvent", "myCallbackValue"), 2000)
